ba7c2742bdcb56726fc8bb63c31857fda9a67397
Ugo Laait <ogadi.ugo@gmail.com>
2025-08-04 14:39:47 +0100

    update firebaseAdmin

--- src/utils/firebaseAdmin.ts
+++ src/utils/firebaseAdmin.ts
@@ -16,7 +16,7 @@ import { getFirestore, FieldValue } from 'firebase-admin/firestore';
 import { ServiceAccount } from 'firebase-admin';
 
 const serviceAccount: ServiceAccount = {
-  projectId: "atinuda-5",
+  projectId: process.env.FIREBASE_PROJECT_ID,
   // private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
   privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
   clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
