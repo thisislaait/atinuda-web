556d2b6286b1405fef155c411a1459ab0e2e20b6
Ugo Laait <ogadi.ugo@gmail.com>
2025-08-04 12:04:07 +0100

    refactor firebase credential with stringify variable

--- src/utils/firebaseAdmin.ts
+++ src/utils/firebaseAdmin.ts
@@ -11,21 +11,36 @@
 // export const db = admin.firestore();
 
 
+// import { initializeApp, cert, getApps } from 'firebase-admin/app';
+// import { getFirestore, FieldValue } from 'firebase-admin/firestore';
+// import { ServiceAccount } from 'firebase-admin';
+
+// const serviceAccount: ServiceAccount = {
+//   projectId: "atinuda-5",
+//   // private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+//   privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+//   clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+//   // clientId: process.env.FIREBASE_CLIENT_ID,
+//   // authUri: "https://accounts.google.com/o/oauth2/auth",
+//   // tokenUri: "https://oauth2.googleapis.com/token",
+//   // auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+//   // client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL,
+// };
+
+// if (!getApps().length) {
+//   initializeApp({
+//     credential: cert(serviceAccount),
+//   });
+// }
+
+// const adminDb = getFirestore();
+
+// export { adminDb, FieldValue };
+
 import { initializeApp, cert, getApps } from 'firebase-admin/app';
 import { getFirestore, FieldValue } from 'firebase-admin/firestore';
-import { ServiceAccount } from 'firebase-admin';
 
-const serviceAccount: ServiceAccount = {
-  projectId: "atinuda-5",
-  // private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-  privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
-  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-  // clientId: process.env.FIREBASE_CLIENT_ID,
-  // authUri: "https://accounts.google.com/o/oauth2/auth",
-  // tokenUri: "https://oauth2.googleapis.com/token",
-  // auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
-  // client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL,
-};
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT as string);
 
 if (!getApps().length) {
   initializeApp({
