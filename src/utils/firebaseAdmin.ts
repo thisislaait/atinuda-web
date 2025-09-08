e8e206ccfc2f3a02de7fc2a56614aa4c556c0e84
Ugo Laait <ogadi.ugo@gmail.com>
2025-09-08 12:48:04 +0100

    unused var

--- src/utils/firebaseAdmin.ts
+++ src/utils/firebaseAdmin.ts
@@ -1,13 +1,13 @@
 // utils/firebaseAdmin.ts
 import { initializeApp, cert, getApps } from 'firebase-admin/app';
 import { getFirestore, FieldValue } from 'firebase-admin/firestore';
-import { ServiceAccount } from 'firebase-admin';
+// import { ServiceAccount } from 'firebase-admin';
 
 // Decode the base64 private key first
-const privateKey = Buffer.from(
-  process.env.FIREBASE_PRIVATE_KEY_B64 || '',
-  'base64'
-).toString('utf8');
+// const privateKey = Buffer.from(
+//   process.env.FIREBASE_PRIVATE_KEY_B64 || '',
+//   'base64'
+// ).toString('utf8');
 
 // Define the service account object
 const serviceAccountJson = Buffer.from(
