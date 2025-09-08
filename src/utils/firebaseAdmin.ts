48e87fe4a53a6f27e3f0efd929e276f93e2bac4c
Ugo Laait <ogadi.ugo@gmail.com>
2025-09-08 12:29:09 +0100

    security: rm servic account keys

--- src/utils/firebaseAdmin.ts
+++ src/utils/firebaseAdmin.ts
@@ -10,12 +10,12 @@ const privateKey = Buffer.from(
 ).toString('utf8');
 
 // Define the service account object
-const serviceAccount: ServiceAccount = {
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  privateKey,
-  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
- 
-};
+const serviceAccountJson = Buffer.from(
+  process.env.FIREBASE_SERVICE_ACCOUNT_B64 || "",
+  "base64"
+).toString("utf8");
+
+const serviceAccount = JSON.parse(serviceAccountJson);
 
 if (!getApps().length) {
   initializeApp({
@@ -26,50 +26,3 @@ if (!getApps().length) {
 const adminDb = getFirestore();
 
 export { adminDb, FieldValue };
-
-// import { initializeApp, cert, getApps } from 'firebase-admin/app';
-// import { getFirestore, FieldValue } from 'firebase-admin/firestore';
-
-// const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT as string);
-
-// if (!getApps().length) {
-//   initializeApp({
-//     credential: cert(serviceAccount),
-//   });
-// }
-
-// const adminDb = getFirestore();
-
-// export { adminDb, FieldValue };
-
-
-// import { initializeApp, cert, getApps } from 'firebase-admin/app';
-// import { getFirestore, FieldValue } from 'firebase-admin/firestore';
-// import { ServiceAccount } from 'firebase-admin';
-
-// if (!getApps().length) {
-//   const privateKey = process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n');
-//   const clientEmail = process.env.FIREBASE_CLIENT_EMAIL;
-//   const projectId = process.env.FIREBASE_PROJECT_ID;
-
-//   if (!privateKey || !clientEmail || !projectId) {
-//     throw new Error(
-//       'Missing Firebase credentials. Ensure FIREBASE_PRIVATE_KEY, FIREBASE_CLIENT_EMAIL, and FIREBASE_PROJECT_ID are set correctly.'
-//     );
-//   }
-
-//   const serviceAccount: ServiceAccount = {
-//     privateKey,
-//     clientEmail,
-//     projectId,
-//   };
-
-//   initializeApp({
-//     credential: cert(serviceAccount),
-//   });
-// }
-
-// const adminDb = getFirestore();
-// export { adminDb, FieldValue };
-
-
